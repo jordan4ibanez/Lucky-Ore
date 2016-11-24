ee28153bb2b0994b0c4d4c4d0fd196ac2cc6e011
jordan4ibanez <jordan4ibanez@gmail.com>
2016-11-24 17:58:29 -0500

    Add in effects

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ Goals:
 
 
 ]]--
+--the ore
 minetest.register_node("lucky_ore:ore", {
 	description = "Nether Ore",
 	tiles = {
@@ -37,4 +38,50 @@ minetest.register_node("lucky_ore:ore", {
 	paramtype = "light",
 	light_source = 5,
 	groups = {cracky = 3},
+	after_destruct = function(pos, oldnode)
+		local item = minetest.add_entity(pos, "lucky_ore:item")
+
+	end
 })
+--calculate the table items for slot machine-esk item display
+local count = 0
+local lucky_table = {}
+for item,def in pairs(minetest.registered_items) do 
+	count = count + 1
+	lucky_table[count] = item
+end
+
+--the entity which shows all the items
+minetest.register_entity("lucky_ore:item",{
+	hp_max = 1,
+	visual="wielditem",
+	visual_size={x=.33,y=.33},
+	collisionbox = {0,0,0,0,0,0},
+	physical=false,
+	textures={"air"},
+	count = 1,
+	on_activate = function(self, staticdata)
+		self.texture = ItemStack("default:dirt_with_grass"):get_name()
+		self.nodename = "default:dirt_with_grass"
+		self.object:set_properties({textures={self.texture}})
+		self.sound = minetest.sound_play("lucky_ore_slot", {
+				max_hear_distance = 30,
+				gain = 10.0,
+				object = self.object,
+				loop = true,
+			})
+	end,
+	on_step = function(self,dtime)
+		
+		self.texture = ItemStack(lucky_table[self.count]):get_name()
+		self.nodename = lucky_table[self.count]
+		self.object:set_properties({textures={self.texture}})
+		
+		self.count = self.count + 1
+		if self.count > count then
+			self.count = 1
+		end
+		print(self.count)
+	end,
+})
+
