78f49d15acab6d639f5b08eda2b829fe6acbd3df
jordan4ibanez <jordan4ibanez@gmail.com>
2016-11-24 18:32:40 -0500

    Name change for ore and remove goals because they are completed

--- init.lua
+++ init.lua
@@ -1,20 +1,6 @@
---[[
-Goals:
-
--use texture from all default ores and make the ore go through each one fast
--make ore glow
--make ore spawn in an entity which displays all items
--make entity go through all items with a mario cart-esk slot machine sound
--make the starting item a random number from how many items there are
--go ding ding ding when item is randomly selected
--have a half chance of a buzzer playing, along with item turning to tnt, and exploding instantly
-
-
-
-]]--
 --the ore
 minetest.register_node("lucky_ore:ore", {
-	description = "Nether Ore",
+	description = "Lucky Ore",
 	tiles = {
 		{
 			name = "lucky_ore.png",
